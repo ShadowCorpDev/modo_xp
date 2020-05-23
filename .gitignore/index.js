3e4be60ec60934dd711a2d15f5c65c4980cc7439
ShadowCorpDev <64375552+ShadowCorpDev@users.noreply.github.com>
2020-05-23 13:43:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,13 +3,6 @@ const Client = new Discord.Client();
 const fs = require('fs');
 const Canvas = require('canvas');
 const Snekfetch = require('snekfetch');
-let xp = JSON.parse(fs.readFileSync('./xp/xp.json', 'utf8'));
-let xp_lol = JSON.parse(fs.readFileSync('./xp/xp_lol.json', 'utf8'));
-let xp_ftn = JSON.parse(fs.readFileSync('./xp/xp_ftn.json', 'utf8'));
-let xp_rl = JSON.parse(fs.readFileSync('./xp/xp_rl.json', 'utf8'));
-let xp_cs = JSON.parse(fs.readFileSync('./xp/xp_cs.json', 'utf8'));
-let xp_min = JSON.parse(fs.readFileSync('./xp/xp_min.json', 'utf8'));
-let xp_war = JSON.parse(fs.readFileSync('./xp/xp_war.json', 'utf8'));
 var prefix = "*";
 
 Client.login(process.env.TOKEN);
